e46a2e1b95d61e75970cc174983d626e35222504
EnergizedAstronaut <174304106+EnergizedAstronaut@users.noreply.github.com>
2026-04-16 16:32:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.listen(PORT, () => {
-  console.log(`Apple TTS Explorer running on port ${PORT}`);
-});
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.listen(PORT, () => console.log(`TTS Studio running on port ${PORT}`));
